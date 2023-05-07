1f985d96ce63108dad7ca68235e7f5e4ea0b0edf
boyla <boyla@msn.cn>
2023-05-07 10:53:25 +0800

    1.init commit

---
+++ model.js
@@ -0,0 +1,199 @@
+var models = [
+    {
+        title: "项目背景",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+
+
+    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+    {
+        title: "初步设计",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },    {
+        title: "可行性分析",
+        contents: [
+            "内容一，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}",
+            "内容二，本项目（{{name}}）位于{{address}},开工时间{{start_date}},截止时间{{end_date}}"],
+        selected: 0,
+        inUse: false
+    },
+];
+// models.unshift({
+// });
+var str = JSON.stringify(models);
+// console.log("cache str:" + str);
+localStorage.setItem('models', str);
